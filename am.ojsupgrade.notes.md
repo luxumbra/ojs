690fd8d683e2574642d06825a76396b3b59134df
luxumbra <hello@crafted.im>
2020-01-29 12:54:27 +0000

    files for upgrade. notes on upgrade info

---
+++ am.ojsupgrade.notes.md
@@ -0,0 +1,14 @@
+### OJS upgrade on Anthropology Matters website
+
+These notes explain the outline of the process of upgrading OJS2.4 to OJS3 and the issues encountered whilst attempting an upgrade.
+
+#### Issues relating to the OJS upgrade
+Regular upgrades to any software are essential to ensuring a secure and stable platform for your work. The longer an upgrade is left, the more complex, time consuming and risky the process of upgrading a system will be.
+
+The Anthropology Matters website is currently running on OJS2.4.6 and the current stable version is OJS3.1.2-4 - with v2.4.6 being released in 2015, 3.1 in 2017, 3.1.2 in Jan 2019 and the latest stable release 3.1.2-4 in December 2019. There were 7 updates to OJS from 2.4.6 to 2.4.8-4, and OJS3.0 to OJS3.1.2-4 had 12 security updates. None of these updates have been applied to the AM system.
+
+OJS recommend performing incremental upgrades from versions 2.3.x and do say that it is possible to do an upgrade straight from 2.4.x to 3.x but version 3.0 was a complete rewrite of OJS and as such, the structure of content is different and special attention must be paid to this during and after the upgrade. A test upgrade is recommended to ensure data integrity and test for any issues in functionality, etc.
+
+Another issue that has come about from the length of time that has passed is that the system requirements for OJS have changed, meaning that in order to perform an effective upgrade and continue to run the website, the server hosting the site needs to be upgraded from PHP5 to PHP7.
+
+With this information in mind and the importance of the data managed by Anthropology Matters, I would recommend ordering an upgrade package direct from the [OJS Upgrade](https://openjournalsystems.com/ojs-upgrade/) page on the OJS website. They quote 900USD to perform an upgrade on a multi-journal system from v2.4 to 3.1.2 but this may be a special case due to the amount of time that has passed and also because the system is not being hosted by OJS. It would be advisable to request a consultation with their team to discuss this.
---
+++ tools/buildpkg.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+#
+# buildpkg.sh
+#
+# Copyright (c) 2013-2015 Simon Fraser University Library
+# Copyright (c) 2003-2015 John Willinsky
+# Distributed under the GNU GPL v2. For full terms see the file docs/COPYING.
+#
+# Script to create an OJS package for distribution.
+#
+# Usage: buildpkg.sh <version> [<tag>] [<patch_dir>]
+#
+#
+
+GITREP=git://github.com/pkp/ojs.git
+
+if [ -z "$1" ]; then
+	echo "Usage: $0 <version> [<tag>-<branch>] [<patch_dir>]";
+	exit 1;
+fi
+
+VERSION=$1
+TAG=$2
+PATCHDIR=${3-}
+PREFIX=ojs
+BUILD=$PREFIX-$VERSION
+TMPDIR=`mktemp -d $PREFIX.XXXXXX` || exit 1
+
+EXCLUDE="dbscripts/xml/data/locale/en_US/sample.xml		\
+dbscripts/xml/data/sample.xml					\
+docs/dev							\
+locale/te_ST							\
+plugins/importexport/duracloud/lib/DuraCloud-PHP/.git		\
+tests								\
+tools/buildpkg.sh						\
+tools/genLocaleReport.sh					\
+tools/genTestLocale.php						\
+tools/startSubmodulesTRAVIS.sh					\
+tools/test							\
+lib/pkp/tests							\
+.git								\
+.travis.yml							\
+lib/pkp/.git							\
+lib/pkp/tools/travis						\
+lib/pkp/tools/mergePullRequest.sh				\
+lib/pkp/lib/swordappv2/.git					\
+lib/pkp/lib/swordappv2/test					\
+plugins/generic/pdfJsViewer/.git"
+
+
+cd $TMPDIR
+
+echo -n "Cloning $GITREP and checking out tag $TAG ... "
+git clone -q -n $GITREP $BUILD || exit 1
+cd $BUILD
+git checkout -q $TAG || exit 1
+echo "Done"
+
+echo -n "Checking out corresponding submodule ... "
+git submodule -q update --init >/dev/null || exit 1
+echo "Done"
+
+echo -n "Checking out submodule submodules ... "
+cd lib/pkp
+git submodule -q update --init >/dev/null || exit 1
+cd ../..
+echo "Done"
+
+echo -n "Preparing package ... "
+cp config.TEMPLATE.inc.php config.inc.php
+find . \( -name .gitignore -o -name .gitmodules -o -name .keepme \) -exec rm '{}' \;
+rm -rf $EXCLUDE
+echo "Done"
+
+cd ..
+
+echo -n "Creating archive $BUILD.tar.gz ... "
+tar -zhcf ../$BUILD.tar.gz $BUILD
+echo "Done"
+
+if [ ! -z "$PATCHDIR" ]; then
+	echo "Creating patches in $BUILD.patch ..."
+	[ -e "../${BUILD}.patch" ] || mkdir "../$BUILD.patch"
+	for FILE in $PATCHDIR/*; do
+		OLDBUILD=$(basename $FILE)
+		OLDVERSION=${OLDBUILD/$PREFIX-/}
+		OLDVERSION=${OLDVERSION/.tar.gz/}
+		echo -n "Creating patch against ${OLDVERSION} ... "
+		tar -zxf $FILE
+		diff -urN $PREFIX-$OLDVERSION $BUILD | gzip -c > ../${BUILD}.patch/$PREFIX-${OLDVERSION}_to_${VERSION}.patch.gz
+		echo "Done"
+	done
+	echo "Done"
+fi
+
+cd ..
+
+rm -r $TMPDIR
